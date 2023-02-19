1e585b0547c52a61be1ed725887fe9fee65902ca
Brian Tu <briantu@Brians-Air.hitronhub.home>
2023-02-18 23:12:57 -0500

    Create getToken api

---
+++ src/pages/api/getToken.js
@@ -0,0 +1,28 @@
+const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
+const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
+const SPOTIFY_REFRESH_TOKEN = process.env.SPOTIFY_REFRESH_TOKEN;
+
+const authOptions = {
+  method: "POST",
+  headers: {
+    Authorization:
+      "Basic " +
+      Buffer.from(SPOTIFY_CLIENT_ID + ":" + SPOTIFY_CLIENT_SECRET).toString(
+        "base64"
+      ),
+    "Content-Type": "application/x-www-form-urlencoded",
+  },
+  body: new URLSearchParams({
+    grant_type: "refresh_token",
+    refresh_token: SPOTIFY_REFRESH_TOKEN,
+  }),
+};
+
+export default async function handler(req, res) {
+  const response = await fetch(
+    "https://accounts.spotify.com/api/token",
+    authOptions
+  );
+  const data = await response.json();
+  res.status(200).json({ access_token: data.access_token });
+}
